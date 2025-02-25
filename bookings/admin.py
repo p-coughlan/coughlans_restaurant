473f6756c288efa762ae396683b3d99940808396
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-02-25 20:20:29 +0000

    Register Customer, Booking, and Review models in the admin interface

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# Import Customer and Booking models
+from .models import Customer, Booking
 
 # Register your models here.
+admin.site.register(Customer) # Register Customer model
+admin.site.register(Booking) # Register Booking model
+
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import Review model
+from .models import Review
 
 # Register your models here.
+admin.site.register(Review) # Register Review model
