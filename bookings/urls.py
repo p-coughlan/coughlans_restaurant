4b2d7233b2dbdbdaeffefdf36df03f3d31646963
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-12 12:32:22 +0000

    Add daily and monthly calendar views; update navigation and styling

--- bookings/urls.py
+++ bookings/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('success/<int:booking_id>/', views.booking_success, name='booking_success'), # th3 <int:booking_id> part of the URL pattern captures the booking_id value from the URL and passes it to the view function as an argument.
     path('manage/<int:booking_id>/', views.manage_booking, name='manage_booking'),
     path('weekly-calendar/', views.weekly_calendar, name='weekly_calendar'),
+    path('daily-calendar/', views.daily_calendar, name='daily_calendar'),
+    path('monthly-calendar/', views.monthly_calendar, name='monthly_calendar'),
     path('lunch-menu/', views.LunchMenuView.as_view(), name='lunch_menu'),
     path('dinner-menu/', views.DinnerMenuView.as_view(), name='dinner_menu'),
     # Path below surpluses the current requirements
